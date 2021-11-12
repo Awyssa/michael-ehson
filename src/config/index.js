768e417f81a3658bd4dbd52bd40f0f1d9e89c6f4
Hendo-getagent <hendo@getagent.co.uk>
2021-11-12 19:46:13 +0000

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -7,14 +7,14 @@ const DefaultSEO = () =>
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<meta charSet="utf-8" />
 			<meta name="robots" content="index,follow" />
-			<meta name="description" content="Michael Henderson, Software Engineer / Full Stack Developer"/>
+			<meta name="description" content="Michael Henderson, Software Engineer / Full Stack Developer, London, UK"/>
 			<meta name="author" content="Michael Henderson" />
 			<meta name="image" content="https://res.cloudinary.com/cornhub/image/upload/v1629756211/picture1.jpg" />
 			<meta property="og:url" content="https://www.m-henderson.com" />
 			<meta property="og:title" content="Michael Henderson" />
-			<meta property="og:description" content="Michael Henderson, Software Engineer / Full Stack Developer" />
+			<meta property="og:description" content="Michael Henderson, Software Engineer / Full Stack Developer, London, UK" />
 			<meta property="og:type" content="website" />
-			<meta property="og:site_name" content="Michael Henderson, Software Engineer / Full Stack Developer" />
+			<meta property="og:site_name" content="Michael Henderson, Software Engineer / Full Stack Developer, London, UK" />
 			<meta property="og:image" content="https://res.cloudinary.com/cornhub/image/upload/v1629756211/picture1.jpg" />
 			<meta property="og:image:alt" content="Michael Henderson" />
 			<meta property="og:image:width" content="1500" />
