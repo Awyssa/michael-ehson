6b62ab5ea748729f6747d40a6e8aaec5b2d09aa6
awyssa <michael.ehson@gmail.com>
2021-12-15 13:43:54 +0000

    I really don't remember what I did this time...

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -6,6 +6,14 @@ import Image from "next/image";
 
 const About = () =>
 {
+
+	const Link = ({href, text}) =>
+	{
+		return <a style={{ color: "#0070FF" }} href={href} target="_blank" rel="noreferrer">
+			{text}
+		</a>;
+	};
+
 	return (
 		<Box
 			width="100%"
@@ -21,14 +29,27 @@ const About = () =>
 						<Image pt="2" width="300" height="300" objectFit="contain" src={myPicture} alt="Michael Henderson" />
 					</Box>
 					<Box px="8">
-						<Heading pb="2">About Me</Heading>
-						<Text py="1">Hi, I'm Michael Henderson!</Text>
-						<Text py="1">I am a software engineer based in London, UK.</Text>
-						<Text py="1">I started coding by trying to mod "Command & Conquer: Tiberian Sun - Firestorm", back when S Club 7 was popular, and the Nokia 3310 had just been released.</Text>
-						<Text py="1">My career started Sales and Marketing a long time ago, but I pursued my true calling and became a developer in 2020. Glad to say, it was the best decision of my life!</Text>
-						<Text py="1">I am currently focusing on React, Node.js, and Next.js, but still practice GraphQL, Django, and Rails.</Text>
+						<Box py="2" as="h1" fontSize="36px" fontWeight="600"> About Me </Box>
+						<Text py="1" fontWeight="700"> Hi, I'm Michael Henderson! </Text>
+						<Text py="1"> I am a Software Engineer / Full Stack Developer based in London, UK. </Text>
+						<Text py="1">
+							My earliest experience with computers was using floppy disks to play Gorillas on my Father's MS-DOS-5. I then started coding by trying to mod "Command & Conquer: Tiberian Sun - Firestorm". This was when S Club 7 was popular, and the Nokia 3310 had just been released.
+						</Text>
+						<Text py="1">
+							After almost a year of trial and error, and many tears, I succeeded in my quest. The utter sense of accomplishment and joy I experienced has led me to become fascinated with computing and coding ever since.
+						</Text>
+						<Text py="1">
+							My career differed from my passion, as I fell into Sales and Marketing. But with a growing disinterest over the years, I finally pursued my true calling and became a developer in 2019. Glad to say, it was the best decision of my life!
+						</Text>
+						<Text py="1">
+							I am currently focusing on React, Node.js, and Next.js, but still practice GraphQL, Django, and Rails.
+						</Text>
 						<Text pt="4px">
-							I also follow the works of Alan Kay, Paul Graham <a style={{ color: "#0070FF" }} href="http://www.paulgraham.com/articles.html" target="_blank" rel="noreferrer">(http://www.paulgraham.com/articles.html)</a>, Oz Nova <a style={{ color: "#0070FF" }} href="https://ozwrites.com" target="_blank" rel="noreferrer">(https://ozwrites.com)</a>, and the Soft Skills Engineering Podcast <a style={{ color: "#0070FF" }} href="https://softskills.audio/" target="_blank" rel="noreferrer">(https://softskills.audio)</a>.
+							I also follow
+							<Link href="http://www.paulgraham.com/articles.html" text=" Paul Graham" />,
+							<Link href="https://ozwrites.com" text=" Oz Nova" />, the
+							<Link href="https://softskills.audio/" text=" Soft Skills Engineering Podcast" />, and going through
+							<Link href="https://cs50.harvard.edu" text=" CS50" /> too.
 						</Text>
 					</Box>
 				</Box>
